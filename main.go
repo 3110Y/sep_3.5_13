6f060351e443ead28ffc6b0f25ce28fde3606b9e
Gaevoy Roman <gaevoyrs@gmail.com>
2020-07-20 20:49:29 +0300

    bad

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"archive/zip"
+	"bytes"
+	"encoding/csv"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 	"strings"
@@ -35,13 +38,12 @@ func (s *SearchValueForZip) searchZip(path string, info os.FileInfo, err error) 
 	}
 	if strings.Contains(info.Name(), ".zip") {
 		s.fileZip = path
-		fmt.Print("searchZip " + s.fileZip)
 	}
-	fmt.Println("searchZip " + s.fileZip)
 	return nil
 }
 
 func (s *SearchValueForZip) unzip() {
+	isFound := false
 	fileZip, err := zip.OpenReader(s.fileZip)
 	if err != nil {
 		panic(fmt.Sprintf("Какая-то ошибка zip: %v\n", err))
@@ -49,9 +51,46 @@ func (s *SearchValueForZip) unzip() {
 	defer fileZip.Close()
 	for _, file := range fileZip.File {
 		if strings.Contains(file.Name, ".txt") {
-			fmt.Printf("Contents of %s:\n", file.Name)
+			if file.CompressedSize64 > 0 {
+				isFound = s.parseFile(file)
+			}
+			if isFound == true {
+				return
+			}
 		}
 	}
+}
+
+func (s *SearchValueForZip) parseFile(file *zip.File) bool {
+	rc, err := file.Open()
+	if err != nil {
+		panic(fmt.Sprintf("Какая-то ошибка Чтения Файла: %v\n", err))
+	}
+	_, err = io.CopyN(os.Stdout, rc, 68)
+	if err != nil {
+		panic(fmt.Sprintf("Какая-то ошибка Копирования Файла: %v\n", err))
+	}
+	rc.Close()
+	content := fmt.Sprint()
+	if content != "" {
+		fmt.Println(file.Name)
+		return s.parseCSV([]byte(content))
+	}
+	return false
+}
+
+func (s *SearchValueForZip) parseCSV(content []byte) bool {
+	buf := bytes.NewBuffer(content)
+	r := csv.NewReader(buf)
+	for i := 1; i <= 2; i++ {
+		// Читать данные мы тоже можем построчно, получая срез строк за каждую итерацию
+		row, err := r.Read()
+		if err != nil && err != io.EOF { // Здесь тоже нужно учитывать конец файла
+			panic(fmt.Sprintf("Какая-то ошибка Разбора csv: %v\n", err))
+		}
+		fmt.Println(row)
+	}
+	return false
 }
 
 func main() {
