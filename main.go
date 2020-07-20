f2eecf95b7f232389430957dcf12282b0c8a91d4
Gaevoy Roman <gaevoyrs@gmail.com>
2020-07-20 21:52:30 +0300

    I try buffer

--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"archive/zip"
-	"bytes"
-	"encoding/csv"
 	"fmt"
 	"io"
 	"os"
@@ -12,19 +10,35 @@ import (
 )
 
 type SearchValueForZip struct {
-	fileZip string
-	value   string
-	root    string
+	findDir  string
+	cacheDir string
+	fileZip  string
+	fileCSV  string
+	value    string
 }
 
 func (s SearchValueForZip) String() string {
-	if s.root == "" {
-		panic("Не установлен root")
+	if s.findDir == "" {
+		panic("Не установлена директория поиска")
 	}
-	if err := filepath.Walk(s.root, s.searchZip); err != nil {
-		panic(fmt.Sprintf("Какая-то ошибка Хождения: %v\n", err))
+	if s.cacheDir == "" {
+		panic("Не установлен директория кеша")
 	}
-	s.unzip()
+	if err := s.dellCache(); err != nil {
+		panic(fmt.Sprintf("Какая-то ошибка удаления: %v\n", err))
+	}
+	if err := filepath.Walk(s.findDir, s.searchZip); err != nil {
+		panic(fmt.Sprintf("Какая-то ошибка поиска: %v\n", err))
+	}
+	if err := s.unzip(); err != nil {
+		panic(fmt.Sprintf("Какая-то ошибка разархивирования: %v\n", err))
+	}
+	if err := filepath.Walk(s.cacheDir, s.findCSV); err != nil {
+		panic(fmt.Sprintf("Какая-то ошибка поиска: %v\n", err))
+	}
+	if err := s.dellCache(); err != nil {
+		panic(fmt.Sprintf("Какая-то ошибка удаления: %v\n", err))
+	}
 	return s.value
 }
 
@@ -42,59 +56,67 @@ func (s *SearchValueForZip) searchZip(path string, info os.FileInfo, err error) 
 	return nil
 }
 
-func (s *SearchValueForZip) unzip() {
-	isFound := false
-	fileZip, err := zip.OpenReader(s.fileZip)
+func (s *SearchValueForZip) unzip() error {
+	reader, err := zip.OpenReader(s.fileZip)
 	if err != nil {
-		panic(fmt.Sprintf("Какая-то ошибка zip: %v\n", err))
+		return err
 	}
-	defer fileZip.Close()
-	for _, file := range fileZip.File {
-		if strings.Contains(file.Name, ".txt") {
-			if file.CompressedSize64 > 0 {
-				isFound = s.parseFile(file)
-			}
-			if isFound == true {
-				return
-			}
+	if err := os.MkdirAll(s.cacheDir, 0755); err != nil {
+		return err
+	}
+
+	for _, file := range reader.File {
+		path := filepath.Join(s.cacheDir, file.Name)
+		if file.FileInfo().IsDir() {
+			os.MkdirAll(path, file.Mode())
+			continue
+		}
+
+		fileReader, err := file.Open()
+		if err != nil {
+			return err
+		}
+		defer fileReader.Close()
+
+		targetFile, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, file.Mode())
+		if err != nil {
+			return err
+		}
+		defer targetFile.Close()
+		if _, err := io.Copy(targetFile, fileReader); err != nil {
+			return err
 		}
 	}
+	return nil
 }
 
-func (s *SearchValueForZip) parseFile(file *zip.File) bool {
-	rc, err := file.Open()
-	if err != nil {
-		panic(fmt.Sprintf("Какая-то ошибка Чтения Файла: %v\n", err))
+func (s *SearchValueForZip) dellCache() error {
+	if _, err := os.Stat(s.cacheDir); err == nil {
+		return os.RemoveAll(s.cacheDir)
 	}
-	_, err = io.CopyN(os.Stdout, rc, 68)
-	if err != nil {
-		panic(fmt.Sprintf("Какая-то ошибка Копирования Файла: %v\n", err))
-	}
-	rc.Close()
-	content := fmt.Sprint()
-	if content != "" {
-		fmt.Println(file.Name)
-		return s.parseCSV([]byte(content))
-	}
-	return false
+	return nil
+
 }
 
-func (s *SearchValueForZip) parseCSV(content []byte) bool {
-	buf := bytes.NewBuffer(content)
-	r := csv.NewReader(buf)
-	for i := 1; i <= 2; i++ {
-		// Читать данные мы тоже можем построчно, получая срез строк за каждую итерацию
-		row, err := r.Read()
-		if err != nil && err != io.EOF { // Здесь тоже нужно учитывать конец файла
-			panic(fmt.Sprintf("Какая-то ошибка Разбора csv: %v\n", err))
-		}
-		fmt.Println(row)
+func (s *SearchValueForZip) findCSV(path string, info os.FileInfo, err error) error {
+	if err != nil {
+		return err // Если по какой-то причине мы получили ошибку, проигнорируем эту итерацию
 	}
-	return false
+	if info.IsDir() {
+		return nil // Проигнорируем директории
+	}
+	if strings.Contains(info.Name(), ".txt") {
+		return s.parseCSV(path)
+	}
+	return nil
+}
+
+func (s *SearchValueForZip) parseCSV(file string) error {
+	return nil
 }
 
 func main() {
-	searchValueForZip := SearchValueForZip{root: "."}
+	searchValueForZip := SearchValueForZip{findDir: ".", cacheDir: "./cache"}
 	fmt.Print(searchValueForZip)
 
 }
